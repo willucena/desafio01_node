30c4f60db8547bdfa7de7a128bbd1797d9ced92e
p21wilson <dev261456>
2020-06-20 23:28:35 -0300

    Desafio finalizado

---
+++ src/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+function validateId(request, response , next){
+  const { id } = request.params;
+  if(!isUuid(id)){
+    return response.status(400).json({erro: 'Invalid project ID'});
+  }
+  return next()
+ }
+
+function updateProperts(request, response , next){
+  const { id } = request.params;
+  next()
+  if(isUuid(id)){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+app.use('/repositories/:id', validateId);
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories)
+});
+
+app.post("/repositories", (request, response) => {
+  const {title, url, techs } = request.body
+  const repository = {
+    id: uuid(), title, url, techs, likes: 0
+  }
+   repositories.push(repository);
+   return response.json(repository);
+});
+
+app.put("/repositories/:id",updateProperts, (request, response) => {
+  const { id } = request.params;
+  const {title, url, techs, likes } = request.body
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  const repository = {
+   id:uuid(), title, url, techs, likes
+  }
+
+  if(repository.likes > 0)
+  {
+     repository.likes = 0;
+  }
+
+  repositories[repositoryIndex] = repository;
+  return response.json(repository)
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.findIndex(repository => repository.id === id );
+
+  repositories.splice(repository, 1);
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.find(repository => repository.id === id );
+  repository.likes += 1; 
+  return response.json(repository)
+});
+
+module.exports = app;
